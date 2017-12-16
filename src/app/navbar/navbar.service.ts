62522da0f0f0d0f4725a1ec56db0324d50a1c7e7
Thomas D Williams <dwilliams@halehan.com>
2017-12-15 19:56:25 -0600

    added more

---
+++ src/app/navbar/navbar.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class NavbarService {
+  vis: boolean;
+  count: number;
+  active: string;
+  homeActive = '';
+  dashActive = 'active';
+  profileActive = '';
+
+    constructor() { this.vis = false; this.count = this.count + 1}
+
+   getHomeActive() {return this.homeActive}
+   getDashActive() {return this.dashActive}
+   getProfileActive() {return this.profileActive}
+
+    hide() { this.vis = false; this.count += 1; }
+
+    show() { this.vis = true; this.count += 1; }
+
+    getVisible() { return this.vis};
+
+    getCount() {return this.count}
+
+}
